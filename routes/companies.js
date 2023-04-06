c88c01cd7482bb23d3bd1aba3a56f62dc612ec31
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-06 13:45:15 -0500

    connected company router to app

---
+++ routes/companies.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+const db = require("../db");
+const router = new express.Router();
+const { BadRequestError, NotFoundError } = require("../expressError");
+
+
+
+
+module.exports = router;
