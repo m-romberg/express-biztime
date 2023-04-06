2bffd1614d50f5f5859564a52ed118b84f8dd500
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-06 12:21:55 -0700

    add route to GET all companies

--- routes/companies.js
+++ routes/companies.js
@@ -5,7 +5,16 @@ const db = require("../db");
 const router = new express.Router();
 const { BadRequestError, NotFoundError } = require("../expressError");
 
+/**
+ * GET /comapnies return list of companies {companies: [{code, name}, ...]}
+ */
 
+router.get("/", async function (req, res) {
+  const results = await db.query("SELECT code, name FROM companies");
+  const companies = results.rows;
+
+  return res.json({ companies });
+});
 
 
 module.exports = router;
