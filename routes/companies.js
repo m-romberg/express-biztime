9e7b940a62377795884bc3bc05c48699f780e2cb
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-06 12:30:42 -0700

    add route to GET one company

--- routes/companies.js
+++ routes/companies.js
@@ -16,5 +16,22 @@ router.get("/", async function (req, res) {
   return res.json({ companies });
 });
 
+/**
+ * GET /comapnies/[code] return company {company: {code, name, description}}
+ */
+
+router.get("/:code", async function (req, res) {
+  const companyCode = req.params.code;
+
+  const results = await db.query(
+    "SELECT code, name, description FROM companies WHERE code = $1",
+    [companyCode]);
+  const company = results.rows[0];
+
+  if (!company) throw new NotFoundError(`No matching company ${companyCode}.`);
+
+  return res.json({ company });
+});
+
 
 module.exports = router;
