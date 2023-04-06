d7d3998b687499f77abd3a185936640ec2798953
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-06 16:56:47 -0700

    add route to query specific invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -23,3 +23,39 @@ router.get("/", async function (req, res) {
 
   return res.json({ invoices });
 });
+
+/**
+ * GET /invoices/[id]
+ * returns: {invoice: {id, amt, paid, add_date, paid_date,
+ *            company: {code, name, description} }}
+ */
+
+router.get("/:id", async function (req, res) {
+  const id = req.params.id;
+
+  const resultsI = await db.query(
+    `SELECT id, amt, paid, add_date, paid_date, comp_code
+      FROM invoices
+      WHERE id = $1`,
+    [id]
+  );
+  const invoice = resultsI.rows[0];
+  if (!invoice) throw new NotFoundError(`No matching invoice ${id}.`);
+
+  const compCode = invoice.comp_code;
+  const resultsC = await db.query(
+    `SELECT code, name, description
+      FROM companies
+      WHERE code = $1`,
+    [compCode]
+  );
+
+  const company = resultsC.rows[0];
+  if (!company) throw new NotFoundError(
+    `No matching invoice with company ${company}`);
+
+  delete invoice.comp_code;
+  invoice.company = company;
+
+  return res.json({ invoice });
+});
