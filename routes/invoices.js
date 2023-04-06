668e477334868f1d6f83f73897557fc91eca8201
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-06 16:24:48 -0700

    add route to query all invoices

--- routes/invoices.js
+++ routes/invoices.js
@@ -8,3 +8,18 @@ const { BadRequestError, NotFoundError } = require("../expressError");
 
 
 module.exports = router;
+
+//id, amt, paid, add_date, paid_date
+
+/**
+ * GET /invoices return list of invoices {invoices: [{id, comp_code}, ...]}
+ */
+router.get("/", async function (req, res) {
+  const results = await db.query(`
+  SELECT id, comp_code
+    FROM invoices`);
+
+  const invoices = results.rows;
+
+  return res.json({ invoices });
+});
